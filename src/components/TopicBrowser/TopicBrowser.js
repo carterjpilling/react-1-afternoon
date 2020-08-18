0feafad37c5d9650ea0e2bb251a1af561e5f0076
Cj <carterjpilling@gmail.com>
2020-08-18 14:17:38 -0600

    initial commit

---
+++ src/components/TopicBrowser/TopicBrowser.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+class TopicBrowser extends Component{
+  constructor(){
+    super()
+  }
+  
+  render(){
+    return (
+      <p> Hello World</p>
+    )
+  }
+}
+
+export default TopicBrowser
